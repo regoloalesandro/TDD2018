5ff89c19f780d9481bb11c680cafce1618177592
regoloalesandro <41971839+regoloalesandro@users.noreply.github.com>
2018-09-18 17:38:30 -0300

    creacion de contarcartas

--- src/Mazo.php
+++ src/Mazo.php
@@ -3,8 +3,15 @@
 namespace TDD;
 
 class Mazo {
-
+  protected $cartas; 
+  public function __construct($cartas) {
+		$this->cartas = $cartas;
+  }
+  public function contarCartas(){
+     return array_count_values($this->cartas);
+  }
   public function mezclar() {
     return TRUE;
   }
+  
 }
