dca3ae8f5aa2ab96df4eae83126138bfa82f38ec
regoloalesandro <41971839+regoloalesandro@users.noreply.github.com>
2018-09-18 17:08:23 -0300

    Update MazoTest.php

--- tests/MazoTest.php
+++ tests/MazoTest.php
@@ -13,13 +13,23 @@ class MazoTest extends TestCase {
         $mazo = new Mazo;
         $this->assertTrue(isset($mazo));
     }
-
+    /**
+     * Valida que se pueda mezclar el mazo.
+     */
     public function testMezclable() {
         $mazo = new Mazo;
 	    $mazo2= $mazo;
 	    $mazo->mezclar();
         $this->assertNotEquals($mazo,$mazo2);
     }
-
+     /**
+     * Valida que se pueda agregar una carta al mazo.
+     */
+    public function testPoseeCartas(){
+    	$mazo = new Mazo;
+	$mazo->agregarcarta();
+	$this->assertTrue(isset($mazo->carta));
+    
+    }
 
 }
