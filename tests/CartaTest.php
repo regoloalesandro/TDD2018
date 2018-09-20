2f817f4ae5130ddd704de09218dc9e252b337e61
Regolo alesandro <regoloalesandro@gmail.com>
2018-09-20 15:43:42 -0300

    agruego test en carta para chequear si se guarda el su numero

--- tests/CartaTest.php
+++ tests/CartaTest.php
@@ -11,8 +11,16 @@ class CartaTest extends TestCase {
      */
     public function testExiste() {
         $numero= 2;
-        $carta = new carta($numero);
+        $carta = new Carta($numero);
         $this->assertTrue(isset($carta));
-    }
 
+    }
+    /**
+     * Valida si se gurada bien el numero en la carta
+     */
+    public function testtienenumero(){
+        $numero = 2;
+        $carta = new Carta($numero);
+        $this->assertEquals($carta->numero(),2);
+    }
 }
