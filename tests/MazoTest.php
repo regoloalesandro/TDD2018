97b69d9f44da777690ad4da6d034917e9c3c553e
Regolo alesandro <regoloalesandrogmail.com>
2018-10-24 16:31:00 -0300

    cambio en Mazotest.php, faltaba un parametro en el assert =equals

--- tests/MazoTest.php
+++ tests/MazoTest.php
@@ -32,7 +32,7 @@ class MazoTest extends TestCase {
 	$mazo = new Mazo($cartas);	    
         $this->assertNotEquals($mazo->mezclar(),$mazo);
         $mazo2= new Mazo(array());
-        $this->assertEquals(null == $mazo2->mezclar());
+        $this->assertEquals($mazo2->mezclar(),null);
     }
      /**
      * Validar si el mazo tien cartas.
